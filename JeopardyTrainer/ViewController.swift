87e18ff87aa1f410794cda141923610e37ddfde4
Kim-An Quinn <kiman.quinn@bc.edu>
2019-12-13 09:36:01 -0500

    Initial Commit

---
+++ JeopardyTrainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JeopardyTrainer
+//
+//  Created by Kim-An Quinn on 12/13/19.
+//  Copyright © 2019 Kim-An Quinn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
