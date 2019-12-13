da6976136e1abc89e6ade334a432599710d851ea
Kim-An Quinn <kiman.quinn@bc.edu>
2019-12-13 11:38:14 -0500

    Jeopardy Trainer Final App

---
+++ JeopardyTrainer/Category.swift
@@ -0,0 +1,26 @@
+//
+//  Category.swift
+//  JeopardyTrainer
+//
+//  Created by Kim-An Quinn on 12/13/19.
+//  Copyright © 2019 Kim-An Quinn. All rights reserved.
+//
+
+import Foundation
+
+class Category{
+    var id: Int
+    var title: String
+    var cluesCount: Int
+
+    init(id: Int, title: String, cluesCount: Int){
+        self.id = id
+        self.title = title
+        self.cluesCount = cluesCount
+    }
+    
+//    var id = 0
+//    var title = ""
+//    var cluesCount = 0
+//
+}
---
+++ JeopardyTrainer/Clue.swift
@@ -0,0 +1,18 @@
+//
+//  Clue.swift
+//  JeopardyTrainer
+//
+//  Created by Kim-An Quinn on 12/13/19.
+//  Copyright © 2019 Kim-An Quinn. All rights reserved.
+//
+
+import Foundation
+
+
+class Clue{
+    var question = ""
+    var answer = ""
+    var categoryID = 0
+    var value = 0
+    var clueID = 0
+}
